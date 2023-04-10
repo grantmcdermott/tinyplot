583cb6bbd67247338891c2d5fd2c5539dcd85826
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2023-04-10 09:11:25 -0400

    liberation font

--- inst/tinytest/helpers.R
+++ inst/tinytest/helpers.R
@@ -1,3 +1,4 @@
 library(tinytest)
 library(tinysnapshot)
 options("tinysnapshot_device" = "svglite")
+options("tinysnapshot_device_args" = list(user_fonts = fontquiver::font_families("Liberation")))

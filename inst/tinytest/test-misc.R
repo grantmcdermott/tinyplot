8aae264b4a14664acf15dc9942ce08117b33ff87
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2023-04-11 22:03:46 +0000

    Add argument `log` (#15)
    * add argument `log`
    
    * `devtools::document()`

---
+++ inst/tinytest/test-misc.R
@@ -0,0 +1,31 @@
+source("helpers.R")
+using("tinysnapshot")
+if (Sys.info()["sysname"] != "Linux") exit_file("Linux snapshots")
+
+f = function() {
+  par(mfrow = c(1, 2))
+  plot(Temp ~ Day, data = airquality, log = "x")
+  plot2(Temp ~ Day, data = airquality, log = "x")
+} 
+expect_snapshot_plot(f, label = "arg_log_x")
+
+f = function() {
+  par(mfrow = c(1, 2))
+  plot(Temp ~ Day, data = airquality, log = "y")
+  plot2(Temp ~ Day, data = airquality, log = "y")
+} 
+expect_snapshot_plot(f, label = "arg_log_y")
+
+f = function() {
+  par(mfrow = c(1, 2))
+  plot(Temp ~ Day, data = airquality, log = "xy")
+  plot2(Temp ~ Day, data = airquality, log = "xy")
+} 
+expect_snapshot_plot(f, label = "arg_log_xy")
+
+f = function() {
+  par(mfrow = c(1, 2))
+  plot(Temp ~ Day, data = airquality, log = "yx")
+  plot2(Temp ~ Day, data = airquality, log = "yx")
+} 
+expect_snapshot_plot(f, label = "arg_log_yx")

41e2b21555b740e33661e77005db0d0da0ad5fe4
Grant McDermott <grant.mcdermott@gmail.com>
2023-04-15 22:18:26 -0700

    Add "by" keyword option

--- R/by_aesthetics.R
+++ R/by_aesthetics.R
@@ -56,13 +56,27 @@ by_col = function(ngrps, col = NULL, palette = NULL, palette.args = NULL) {
 }
 
 
-by_pch = function(ngrps, type, pch) {
+by_pch = function(ngrps, type, pch=NULL) {
   
   no_pch = FALSE
-  # return NULL if not a valid point type
   if (!type %in% c("p", "b", "o")) {
     no_pch = TRUE
     pch = NULL
+    
+    # special "by" convenience keyword
+  } else if (!is.null(pch) && length(pch)==1 && pch=="by") {
+    no_pch = TRUE # skip checks below
+    pch = 1:ngrps + par("pch") - 1
+    # correctly recycle if over max pch type
+    pch_ceiling = 25 # see ?pch
+    if (max(pch)>pch_ceiling) {
+      pch_below = pch[pch<=pch_ceiling]
+      pch_above = pch[pch>pch_ceiling]
+      pch_above = rep_len(0:pch_ceiling, length(pch_above))
+      pch = c(pch_below, pch_above)
+    }
+    
+    # return NULL if not a valid point type
   } else if (is.null(pch)) {
     pch = par("pch")
   }
@@ -82,13 +96,39 @@ by_pch = function(ngrps, type, pch) {
 }
 
 
-by_lty = function(ngrps, type, lty) {
+by_lty = function(ngrps, type, lty=NULL) {
 
   # don't care about line type, return NULL
   if (!type %in% c("l", "b", "o", "c", "h", "s", "S")) {
     out = NULL
-
-  # NULL -> solid line
+    
+    # special "by" convenience keyword
+  } else if (!is.null(lty) && length(lty)==1 && lty=="by") {
+    
+    lty_dict = c("solid", "dashed", "dotted", "dotdash", "longdash", "twodash")
+    par_lty = par("lty")
+    
+    if (!par_lty %in% lty_dict) {
+      warning(
+        "\nBesoke lty specifications (i.e., using string combinations) are not",
+        "currently supported alongside the lty='by' keyword argument.",
+        "Defaulting to 1 and looping from there.\n"
+        )
+      par_lty = 1
+    } else {
+      par_lty = which(par_lty==lty_dict)
+    }
+    out = 1:ngrps + par_lty - 1
+    # correctly recycle if over max pch type
+    lty_ceiling = 6 # see ?pch
+    if (max(out)>lty_ceiling) {
+      lty_below = out[out<=lty_ceiling]
+      lty_above = out[out>lty_ceiling]
+      lty_above = rep_len(1:lty_ceiling, length(lty_above))
+      out = c(lty_below, lty_above)
+    }
+    
+    # NULL -> solid (or default) line
   } else if (is.null(lty)) {
     out = rep(par("lty"), ngrps)
   

01a984fe29eba5b310f133e7946dba3139988db4
Grant McDermott <grant.mcdermott@gmail.com>
2023-05-26 17:18:10 -0700

    More tests for legend arg

---
+++ inst/tinytest/test-legend.R
@@ -0,0 +1,41 @@
+source("helpers.R")
+using("tinysnapshot")
+if (Sys.info()["sysname"] != "Linux") exit_file("Linux snapshots")
+
+# position keyword examples 
+
+f = function() with(airquality, plot2(x = Day, y = Temp, by = Month, legend = "bottom!"))
+expect_snapshot_plot(f, label = "legend_default_keyword")
+
+f = function() with(airquality, plot2(x = density(Temp), by = Month, legend = "bottom!"))
+expect_snapshot_plot(f, label = "legend_density_keyword")
+
+f = function() plot2(Temp ~ Day | Month, data = airquality, legend = "bottom!")
+expect_snapshot_plot(f, label = "legend_formula_keyword")
+
+
+# legend function examples
+
+f = function() with(
+  airquality,
+  plot2(
+    x = Day, y = Temp, by = Month,
+    legend = legend("bottom!", title = "Month of the year", bty = "o")
+    )
+  )
+expect_snapshot_plot(f, label = "legend_default_legend")
+
+f = function() with(
+  airquality,
+  plot2(
+    x = density(Temp), by = Month,
+    legend = legend("bottom!", title = "Month of the year", bty = "o")
+    )
+  )
+expect_snapshot_plot(f, label = "legend_density_legend")
+
+f = function() plot2(
+  Temp ~ Day | Month, data = airquality,
+  legend = legend("bottom!", title = "Month of the year", bty = "o")
+  )
+expect_snapshot_plot(f, label = "legend_formula_legend")

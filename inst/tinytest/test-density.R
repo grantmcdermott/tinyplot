17c6029416482510dda795c3173cd5c8e09cfa28
Grant McDermott <grant.mcdermott@gmail.com>
2023-06-18 23:26:13 -0700

    Skip density plot tests in R dev (#36)
    - To help with failing CI tests on development run.
    - See https://bugs.r-project.org/show_bug.cgi?id=18337

--- inst/tinytest/test-density.R
+++ inst/tinytest/test-density.R
@@ -2,6 +2,13 @@ source("helpers.R")
 using("tinysnapshot")
 if (Sys.info()["sysname"] != "Linux") exit_file("Linux snapshots")
 
+## Sidestep test fails due to new (R 4.4.0) density grid value calculations.
+## https://bugs.r-project.org/show_bug.cgi?id=18337
+exit_if_not(getRversion()  <= "4.3.1")
+## Note: Once 4.4.0 is released we can either generate some new plots or
+## test with something like:
+# f = function() plot2(density(mtcars$mpg, old.coords=TRUE))
+
 f = function() plot2(density(mtcars$mpg))
 expect_snapshot_plot(f, label = "density_nogroups")
 

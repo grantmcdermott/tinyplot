ead56054f140c56a76ea910b97352d8437913bb6
Grant McDermott <grant.mcdermott@gmail.com>
2023-06-18 22:31:08 -0700

    Try a different tack

--- inst/tinytest/test-density.R
+++ inst/tinytest/test-density.R
@@ -2,26 +2,37 @@ source("helpers.R")
 using("tinysnapshot")
 if (Sys.info()["sysname"] != "Linux") exit_file("Linux snapshots")
 
-if ((getRversion() > "4.3.1")) {
+exit_if_not(getRVersion()  <= "4.3.1")
 
-  f = function() plot2(density(mtcars$mpg, old.coords=TRUE))
-  expect_snapshot_plot(f, label = "density_nogroups")
-  
-  f = function() plot2(density(mtcars$mpg, old.coords=TRUE), by = mtcars$am)
-  expect_snapshot_plot(f, label = "density_numeric")
-  
-  f = function() plot2(density(iris$Sepal.Width, old.coords=TRUE), by = iris$Species)
-  expect_snapshot_plot(f, label = "density_factor")
-  
-} else {
+f = function() plot2(density(mtcars$mpg))
+expect_snapshot_plot(f, label = "density_nogroups")
 
-  f = function() plot2(density(mtcars$mpg))
-  expect_snapshot_plot(f, label = "density_nogroups")
-  
-  f = function() plot2(density(mtcars$mpg), by = mtcars$am)
-  expect_snapshot_plot(f, label = "density_numeric")
-  
-  f = function() plot2(density(iris$Sepal.Width), by = iris$Species)
-  expect_snapshot_plot(f, label = "density_factor")
+f = function() plot2(density(mtcars$mpg), by = mtcars$am)
+expect_snapshot_plot(f, label = "density_numeric")
 
-}
+f = function() plot2(density(iris$Sepal.Width), by = iris$Species)
+expect_snapshot_plot(f, label = "density_factor")
+
+# if ((getRversion() > "4.3.1")) {
+
+#   f = function() plot2(density(mtcars$mpg, old.coords=TRUE))
+#   expect_snapshot_plot(f, label = "density_nogroups")
+  
+#   f = function() plot2(density(mtcars$mpg, old.coords=TRUE), by = mtcars$am)
+#   expect_snapshot_plot(f, label = "density_numeric")
+  
+#   f = function() plot2(density(iris$Sepal.Width, old.coords=TRUE), by = iris$Species)
+#   expect_snapshot_plot(f, label = "density_factor")
+  
+# } else {
+
+#   f = function() plot2(density(mtcars$mpg))
+#   expect_snapshot_plot(f, label = "density_nogroups")
+  
+#   f = function() plot2(density(mtcars$mpg), by = mtcars$am)
+#   expect_snapshot_plot(f, label = "density_numeric")
+  
+#   f = function() plot2(density(iris$Sepal.Width), by = iris$Species)
+#   expect_snapshot_plot(f, label = "density_factor")
+
+# }

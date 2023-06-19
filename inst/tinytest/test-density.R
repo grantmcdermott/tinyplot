4c9b9bf78ed79fb8516f66d9642e9283d0813a90
Grant McDermott <grant.mcdermott@gmail.com>
2023-06-18 18:13:43 -0700

    Update test-density.R

--- inst/tinytest/test-density.R
+++ inst/tinytest/test-density.R
@@ -2,11 +2,26 @@ source("helpers.R")
 using("tinysnapshot")
 if (Sys.info()["sysname"] != "Linux") exit_file("Linux snapshots")
 
-f = function() plot2(density(mtcars$mpg), old.coords=TRUE)
-expect_snapshot_plot(f, label = "density_nogroups")
+if ((getRversion() > "4.3.1") {
 
-f = function() plot2(density(mtcars$mpg), by = mtcars$am, old.coords=TRUE)
-expect_snapshot_plot(f, label = "density_numeric")
+  f = function() plot2(density(mtcars$mpg, old.coords=TRUE))
+  expect_snapshot_plot(f, label = "density_nogroups")
+  
+  f = function() plot2(density(mtcars$mpg, old.coords=TRUE), by = mtcars$am)
+  expect_snapshot_plot(f, label = "density_numeric")
+  
+  f = function() plot2(density(iris$Sepal.Width, old.coords=TRUE, by = iris$Species)
+  expect_snapshot_plot(f, label = "density_factor")
+  
+} else {
 
-f = function() plot2(density(iris$Sepal.Width), by = iris$Species, old.coords=TRUE)
-expect_snapshot_plot(f, label = "density_factor")
+  f = function() plot2(density(mtcars$mpg)
+  expect_snapshot_plot(f, label = "density_nogroups")
+  
+  f = function() plot2(density(mtcars$mpg), by = mtcars$am)
+  expect_snapshot_plot(f, label = "density_numeric")
+  
+  f = function() plot2(density(iris$Sepal.Width, by = iris$Species)
+  expect_snapshot_plot(f, label = "density_factor")
+
+}

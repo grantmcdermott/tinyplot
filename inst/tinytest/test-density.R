5c8d2770d272e61b027e2444965ecc65baab6ce5
Grant McDermott <grant.mcdermott@gmail.com>
2023-06-18 23:00:41 -0700

    tidy up

--- inst/tinytest/test-density.R
+++ inst/tinytest/test-density.R
@@ -2,7 +2,12 @@ source("helpers.R")
 using("tinysnapshot")
 if (Sys.info()["sysname"] != "Linux") exit_file("Linux snapshots")
 
+## Sidestep test fails due to new (R 4.4.0) density grid value calculations.
+## https://bugs.r-project.org/show_bug.cgi?id=18337
 exit_if_not(getRversion()  <= "4.3.1")
+## Note: Once 4.4.0 is released we can either generate some new plots or
+## test with something like:
+# f = function() plot2(density(mtcars$mpg, old.coords=TRUE))
 
 f = function() plot2(density(mtcars$mpg))
 expect_snapshot_plot(f, label = "density_nogroups")
@@ -12,27 +17,3 @@ expect_snapshot_plot(f, label = "density_numeric")
 
 f = function() plot2(density(iris$Sepal.Width), by = iris$Species)
 expect_snapshot_plot(f, label = "density_factor")
-
-# if ((getRversion() > "4.3.1")) {
-
-#   f = function() plot2(density(mtcars$mpg, old.coords=TRUE))
-#   expect_snapshot_plot(f, label = "density_nogroups")
-  
-#   f = function() plot2(density(mtcars$mpg, old.coords=TRUE), by = mtcars$am)
-#   expect_snapshot_plot(f, label = "density_numeric")
-  
-#   f = function() plot2(density(iris$Sepal.Width, old.coords=TRUE), by = iris$Species)
-#   expect_snapshot_plot(f, label = "density_factor")
-  
-# } else {
-
-#   f = function() plot2(density(mtcars$mpg))
-#   expect_snapshot_plot(f, label = "density_nogroups")
-  
-#   f = function() plot2(density(mtcars$mpg), by = mtcars$am)
-#   expect_snapshot_plot(f, label = "density_numeric")
-  
-#   f = function() plot2(density(iris$Sepal.Width), by = iris$Species)
-#   expect_snapshot_plot(f, label = "density_factor")
-
-# }

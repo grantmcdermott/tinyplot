12a0440623aac8db207680bd22444c99380c05ba
Grant McDermott <grant.mcdermott@gmail.com>
2023-06-18 17:53:04 -0700

    Backwards compatibility for density plots
    - To help with failing CI tests on development run.
    - See https://bugs.r-project.org/show_bug.cgi?id=18337

--- inst/tinytest/test-density.R
+++ inst/tinytest/test-density.R
@@ -2,11 +2,11 @@ source("helpers.R")
 using("tinysnapshot")
 if (Sys.info()["sysname"] != "Linux") exit_file("Linux snapshots")
 
-f = function() plot2(density(mtcars$mpg))
+f = function() plot2(density(mtcars$mpg), old.coords=TRUE)
 expect_snapshot_plot(f, label = "density_nogroups")
 
-f = function() plot2(density(mtcars$mpg), by = mtcars$am)
+f = function() plot2(density(mtcars$mpg), by = mtcars$am, old.coords=TRUE)
 expect_snapshot_plot(f, label = "density_numeric")
 
-f = function() plot2(density(iris$Sepal.Width), by = iris$Species)
+f = function() plot2(density(iris$Sepal.Width), by = iris$Species, old.coords=TRUE)
 expect_snapshot_plot(f, label = "density_factor")

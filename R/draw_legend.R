ccaa3bc5774c891d6315137a79e9ca618c531b3e
Grant McDermott <grant.mcdermott@gmail.com>
2023-12-28 12:04:46 -0800

    First stab at integrating fixed margins

--- R/draw_legend.R
+++ R/draw_legend.R
@@ -14,7 +14,13 @@
 #' @param col Plotting colour(s), passed down from `plot2`.
 #' @param bg Plotting character background fill colour(s), passed down from `plot2`.
 #' @param cex Plotting character expansion(s), passed down from `plot2`.
-#' @param new_plot Should we be calling plot.new internally? 
+#' @param lmar Legend margins (in lines). Should be a numeric vector of length
+#'   two, where the first number represents the "inner" margin between the
+#'   legend and the plot, and the second number represents the "outer" margin
+#'   between the legend and edge of the graphics device. If no explicit value is
+#'   provided by the user, then reverts back to `par2("lmar")` for which the
+#'   default values are c(1.1, 0.1).
+#' @param new_plot Should we be calling plot.new internally?
 draw_legend = function(
     legend = NULL,
     legend.args = NULL,
@@ -26,8 +32,15 @@ draw_legend = function(
     col = NULL,
     bg = NULL,
     cex = NULL,
+    lmar = NULL,
     new_plot = TRUE
 ) {
+  
+  if (is.null(lmar)) {
+    lmar = par2("lmar")
+  } else {
+    if (!is.numeric(lmar) || length(lmar)!=2) stop ("lmar must be a numeric of length 2.")
+  }
   
   w = h = outer_right = outer_bottom = NULL
   
@@ -97,12 +110,12 @@ draw_legend = function(
     
     outer_right = grepl("right!$", legend.args[["x"]])
     
-    # Margins of the plot (the first is the bottom margin)
-    if (outer_right) {
-      par(mar=c(par("mar")[1:3], 0.1)) # remove right inner margin space
-    } else if (par("mar")[4]==0.1) {
-      par(mar=c(par("mar")[1:3], 2.1)) # revert right margin if outer left
-    }
+    # # Margins of the plot (the first is the bottom margin)
+    # if (outer_right) {
+    #   par(mar=c(par("mar")[1:3], 0.1)) # remove right inner margin space
+    # } else if (par("mar")[4]==0.1) {
+    #   par(mar=c(par("mar")[1:3], 2.1)) # revert right margin if outer left
+    # }
     
     if (isTRUE(new_plot)) plot.new()
     
@@ -121,16 +134,49 @@ draw_legend = function(
     fklgnd = do.call("legend", fklgnd.args)
     # calculate side margin width in ndc
     w = grconvertX(fklgnd$rect$w, to="ndc") - grconvertX(0, to="ndc")
+    # Add another additional space to the side (i.e. as part of the outer margin)
+    w = w + grconvertX(lmar[2], from = "lines", to="ndc") 
+    cat(w, "\n")
     ## differing adjustments depending on side
     if (outer_right) {
-      w = w*1.5
-      par(omd = c(0, 1-w, 0, 1))
-      legend.args[["inset"]] = c(1.025, 0)
+      par(mar=c(par("mar")[1:3], lmar[1]))
+      par(oma = c(par("oma")[1:3], grconvertX(fklgnd$rect$w, to="lines") - grconvertX(0, to="lines") + lmar[2]))
+    w = grconvertX(fklgnd$rect$w, to="ndc") - grconvertX(0, to="ndc")
+    # Add another additional space to the side (i.e. as part of the outer margin)
+    w = w + grconvertX(lmar[2], from = "lines", to="ndc") 
+      # wbump = grconvertX(lmar[1]-lmar[2], from="lines", to="ndc") ## nic same as ndc?
+      # wbump = grconvertX(lmar[1], from="lines", to="ndc") ## nic same as ndc?
+      # legend.args[["inset"]] = c(1+wbump, 0)
+      ### TRY OUTSIDE
+      # legend.args[["inset"]] = c(1+wbump, 0)
+      ### TRY OUTSIDE
+      ## Bump external margin (this is where the legend will be printed)
+      # par(omd = c(0, 1-w, 0, 1))
+      # par(oma = c(par("oma")[1:3], grconvertX(fklgnd$rect$w, to="lines") - grconvertX(0, to="lines") + lmar[2]))
+      # box("figure")
+      ### END TEST
     } else {
-      w = w + grconvertX(par("mgp")[1], from = "lines", to = "ndc") # extra space for y-axis title
+      # avoid recursive indentation
+      if (par("mar")[2] != lmar[1] + sum(par("mgp"))) {
+        par(mar=c(par("mar")[1], lmar[1] + sum(par("mgp")), par("mar")[3:4]))
+      }
+      # extra space for y-axis title and axis labels
+      ytisp = grconvertX(sum(par("mgp")), from = "lines", to = "ndc")
+      # extra space for y-axis title
+      # w = w + grconvertX(par("mgp")[1], from = "lines", to = "ndc")
+      # wbump = grconvertX(lmar[1], from="lines", to="nic") ## nic same as ndc?
+      # wbump = wbump + ytisp
+      wbump = ytisp
+      wbump = wbump + grconvertX(lmar[1], from="lines", to="ndc") 
+      legend.args[["inset"]] = c(1+wbump, 0)
+      # wbump = grconvertX(4, from = "lines", to = "ndc")
       par(omd = c(w, 1, 0, 1))
-      legend.args[["inset"]] = c(1+w, 0)
+      box("figure") ## TEST
+      # legend.args[["inset"]] = c(1.125, 0)
     }
+      wbump = grconvertX(lmar[1], from="lines", to="ndc") ## nic same as ndc?
+      legend.args[["inset"]] = c(1+wbump, 0)
+    # legend.args[["inset"]] = c(1+wbump, 0)
     
     ## Legend at the outer top or bottom of plot
   } else if (grepl("bottom!$|top!$", legend.args[["x"]])) {
@@ -165,11 +211,20 @@ draw_legend = function(
     )
     fklgnd = do.call("legend", fklgnd.args)
     # calculate bottom margin height in ndc
-    h = grconvertX(fklgnd$rect$h, to="ndc") - grconvertX(0, to="ndc")
+    h = grconvertY(fklgnd$rect$h, to="ndc") - grconvertY(0, to="ndc")
     ## differing adjustments depending on side
     if (outer_bottom) {
-      legend.args[["inset"]] = c(0, 1+2*h)
-      par(omd = c(0,1,0+h,1))
+      ## We need extra adjustment to account for the x axis title, so that it
+      ## doesn't overlap the legend. We'll also add an extra 3.1 lines worth of
+      ## spacing between them.
+      xtra_gap = 3.1
+      hbump = sum(par("mgp")[1], xtra_gap)
+      hbump = grconvertY(hbump, from="lines", to="ndc")
+      legend.args[["inset"]] = c(0, 1+hbump)
+      ## Bump external margin (this is where the legend will be printed)
+      h = h + grconvertY(0.1, from="lines", to="ndc")
+      par(omd = c(0, 1, 0+h, 1))
+      # box("figure")
     } else {
       legend.args[["inset"]] = c(0, 1)
       par(omd = c(0,1,0,1-h))
@@ -183,6 +238,15 @@ draw_legend = function(
   }
   
   do.call("legend", legend.args)
+  
+  # if (outer_bottom) {
+  #   # omar = par("mar")
+  #   # par(mar = c(0.1, omar[2:4]))
+  #   hl = grconvertY(h, to="lines", from="ndc")
+  #   cat(hl)
+  #   par(oma = c(hl, par("oma")[2:4]))
+  #   # par(mar=omar)
+  # }
   
 }
 

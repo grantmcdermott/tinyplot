541671206ad78f0497531e4b82af3da199f13717
Grant McDermott <grant.mcdermott@gmail.com>
2024-01-15 22:15:23 -0800

    Tweak plot2 lmar

--- R/zzz.R
+++ R/zzz.R
@@ -11,7 +11,7 @@
   .par2$last_facet_par <- if(is.null(getOption("plot2_last_facet_par"))) NULL else getOption("plot2_last_facet_par")
   
   # Legend margin, i.e. gap between the legend and the plot elements
-  .par2$lmar <- if(is.null(getOption("plot2_lmar"))) c(0.6, 0.5) else as.numeric(getOption("plot2_lmar"))
+  .par2$lmar <- if(is.null(getOption("plot2_lmar"))) c(1.0, 0.1) else as.numeric(getOption("plot2_lmar"))
   
   assign(".par2", .par2, envir = plt2ptns)
   

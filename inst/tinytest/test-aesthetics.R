f128a5aa5ea282a064df5eb3e34c02e06996a94e
Grant McDermott <grant.mcdermott@gmail.com>
2024-03-08 16:13:46 -0800

    Update / add lwd to by_aesthestics tests

--- inst/tinytest/test-aesthetics.R
+++ inst/tinytest/test-aesthetics.R
@@ -18,6 +18,9 @@ expect_snapshot_plot(f, label = "aesthetics_type_b")
 f = function() tinyplot(Temp ~ Day | Month, data = aq, type = "b", lty = 1:5)
 expect_snapshot_plot(f, label = "aesthetics_type_b_lty")
 
+f = function() tinyplot(Temp ~ Day | Month, data = aq, type = "b", lwd = 1:5)
+expect_snapshot_plot(f, label = "aesthetics_type_b_lwd")
+
 f = function() tinyplot(Temp ~ Day | Month, data = aq, type = "b", col = "black", pch = 1:5)
 expect_snapshot_plot(f, label = "aesthetics_type_b_col_pch")
 
@@ -29,28 +32,28 @@ expect_snapshot_plot(f, label = "aesthetics_type_l")
 # Next, convenience versions using the "by" keyword
 # 
 
-f = function() tinyplot(Temp ~ Day | Month, data = aq, type = "b", pch = "by", lty = "by")
+f = function() tinyplot(Temp ~ Day | Month, data = aq, type = "b", pch = "by", lty = "by", lwd = "by")
 expect_snapshot_plot(f, label = "aesthetics_by")
 
 # check non-point types don't generate points in legend accidentally
-f = function() tinyplot(Temp ~ Day | Month, data = aq, type = "l", pch = "by", lty = "by")
+f = function() tinyplot(Temp ~ Day | Month, data = aq, type = "l", pch = "by", lty = "by", lwd = "by")
 expect_snapshot_plot(f, label = "aesthetics_by_type_l")
 
 # no color version
-f = function() tinyplot(Temp ~ Day | Month, data = aq, type = "b", pch = "by", lty = "by", col = "black")
+f = function() tinyplot(Temp ~ Day | Month, data = aq, type = "b", pch = "by", lty = "by", lwd = "by", col = "black")
 expect_snapshot_plot(f, label = "aesthetics_by_nocol")
 
 # inherit global par
 f = function() {
   par(pch = 16)
-  tinyplot(Temp ~ Day | Month, data = aq, type = "b", pch = "by", lty = "by")
+  tinyplot(Temp ~ Day | Month, data = aq, type = "b", pch = "by", lty = "by", lwd = "by")
 }
 expect_snapshot_plot(f, label = "aesthetics_by_par")
 
 # as above but check recycling
 f = function() {
-  par(pch = 25, lty = 6)
-  tinyplot(Temp ~ Day | Month, data = aq, type = "b", pch = "by", lty = "by")
+  par(pch = 25, lty = 6, lwd = 0.5)
+  tinyplot(Temp ~ Day | Month, data = aq, type = "b", pch = "by", lty = "by", lwd = "by")
 }
 expect_snapshot_plot(f, label = "aesthetics_by_recycle")
 

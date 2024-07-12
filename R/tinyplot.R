635e8f9d1515a4c0e4a408667a9982f908201d31
Grant McDermott <grant.mcdermott@gmail.com>
2024-07-11 21:13:27 -0700

    namespace

--- R/tinyplot.R
+++ R/tinyplot.R
@@ -265,7 +265,7 @@
 #' without causing unexpected changes to the output.
 #'   
 #' @importFrom grDevices adjustcolor colorRampPalette extendrange palette palette.colors palette.pals hcl.colors hcl.pals xy.coords png jpeg pdf svg dev.off dev.new dev.list
-#' @importFrom graphics abline arrows axis Axis box boxplot grconvertX grconvertY lines par plot.default plot.new plot.window points polygon polypath segments title mtext text rect
+#' @importFrom graphics abline arrows axis Axis box boxplot grconvertX grconvertY hist lines mtext par plot.default plot.new plot.window points polygon polypath segments rect text title
 #' @importFrom utils modifyList head tail
 #' @importFrom stats na.omit
 #' @importFrom tools file_ext

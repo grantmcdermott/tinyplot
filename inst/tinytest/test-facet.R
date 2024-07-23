2705cb60819c8d2eecf6e0dcce525dedfc1f5c58
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-07-23 07:41:50 -0400

    exit_if_not

--- inst/tinytest/test-facet.R
+++ inst/tinytest/test-facet.R
@@ -273,7 +273,7 @@ tpar(op)
 ## Avoid test fails on older R versions (pre 4.4.0) due to slight change in
 ## density grid value calculations.
 ## https://bugs.r-project.org/show_bug.cgi?id=18337
-exit_if_not(getRversion() >= "4.4.0")
+if (getRversion() < "4.4.0") exit_file("R < 4.4.0")
 
 f = function() {
   with(

3aba9b0dd14fd49daa859d03a654c8137ec5f78d
Grant McDermott <grant.mcdermott@gmail.com>
2024-07-22 17:37:53 -0700

    additional env var catch

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,7 +1,7 @@
 
 # Don't run tests on CRAN (because of test snapshot suite size)
 # See https://github.com/grantmcdermott/tinyplot/pull/128
-NOT_CRAN = toupper(Sys.getenv("NOT_CRAN")) == "TRUE"
+NOT_CRAN = toupper(Sys.getenv("NOT_CRAN")) == "TRUE" || isTRUE(Sys.getenv("NOT_CRAN"))
 
 # Only test on Linux machines (for tinysnapshot fidelity)
 ON_LINUX = Sys.info()["sysname"] == "Linux"

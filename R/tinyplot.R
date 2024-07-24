07ae557f67b89e4f816bddcaeffc5607035163db
Grant McDermott <grant.mcdermott@gmail.com>
2024-07-23 18:29:37 -0700

    Fix #175

--- R/tinyplot.R
+++ R/tinyplot.R
@@ -1521,7 +1521,8 @@ tinyplot.default = function(
         staplewex_xx = ifelse(!is.null(dots[["staplewex"]]), dots[["staplewex"]], 0.5)
         outwex_xx = ifelse(!is.null(dots[["outwex"]]), dots[["outwex"]], 0.5)
         at_xx = unique(xx)
-        if (!is.null(by) && isFALSE(facet_by) && length(split_data)>1) {
+        x_by = !is.null(by) && identical(xlvls, lgnd_labs)
+        if (!is.null(by) && isFALSE(x_by) && isFALSE(facet_by) && length(split_data)>1) {
           boxwex_xx_orig = boxwex_xx
           boxwex_xx = boxwex_xx / length(split_data) - 0.01
           at_xx = at_xx + seq(-((boxwex_xx_orig-boxwex_xx)/2), ((boxwex_xx_orig-boxwex_xx)/2), length.out = length(split_data))[i]

faddcc2ca26bffeb3498e07af437b7e036d025af
Grant McDermott <grant.mcdermott@gmail.com>
2024-07-30 17:41:47 -0700

    type = NULL (#179)
    * switch to type = NULL
    
    * run through type sanitizer
    
    * redundant linux check
    
    * oops
    
    - need to keep alias for formula method before passing type sanitizer
    
    * docs

--- R/sanitize.R
+++ R/sanitize.R
@@ -6,12 +6,18 @@ sanitize_ribbon.alpha = function(ribbon.alpha) {
 
 
 
-
 sanitize_type = function(type, x, y) {
-    # enforce boxplot type for y ~ factor(x)
-    if (!is.null(x) && is.factor(x) && !is.factor(y) && !identical(type, "boxplot")) {
-        type = "boxplot"
-        warning('The `type` argument was changed to "boxplot" automatically because `x` is a factor but not `y`.', call. = FALSE)
+    if (is.null(type)) {
+        # enforce boxplot type for y ~ factor(x)
+        if (!is.null(x) && is.factor(x) && !is.factor(y)) {
+            type = "boxplot"
+        } else {
+            type = "p"
+        }
+    } else if (type %in% c("hist", "histogram")) {
+        type = "histogram"
+    } else if (type %in% c("j", "jitter")) {
+        type = "jitter"
     }
     return(type)
 }

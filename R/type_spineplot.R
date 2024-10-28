26e5635b1cd29b6246e0e07473c61b5d9881abef
Grant McDermott <grant.mcdermott@gmail.com>
2024-10-28 11:26:50 -0700

    refactor:
    - move all data prep to data_spineplot() to leverage standard by+factor logic.
    - pass spineplot-specific extras (e.g., x(y)axlabels, grayscale flag, etc.) to spineplot_draw() via type_info list.
    - adjust to_hcl()/seq_palette() to account for potential alpha transparency.
    - require customization arguments like 'breaks' and 'weights' to be passed through type_spineplot().
    - add some examples.

--- R/type_spineplot.R
+++ R/type_spineplot.R
@@ -1,120 +1,63 @@
 #' Spineplot and spinogram type
 #'
 #' @inheritParams graphics::spineplot
+#' @examples
+#' # "spineplot" type convenience string
+#' tinyplot(Species ~ Sepal.Width, data = iris, type = "spineplot")
+#' 
+#' # Use `type_spineplot()` to pass extra arguments for customization
+#' tinyplot(Species ~ Sepal.Width, data = iris, type = type_spineplot(breaks = 4))
+#' p = palette.colors(3, "Pastel 1")
+#' tinyplot(Species ~ Sepal.Width, data = iris, type = type_spineplot(breaks = 4, col = p))
+#' 
+#' # Grouped and faceted spineplots
+#' 
+#' ttnc = as.data.frame(Titanic)
+#' tinyplot(
+#'   Survived ~ Sex, facet = ~ Class, data = ttnc,
+#'   type = type_spineplot(weights = ttnc$Freq)
+#' )
+#' 
+#' # For grouped "by" spineplots, it's better visually to facet as well
+#' tinyplot(
+#'   Survived ~ Sex | Class, facet = "by", data = ttnc,
+#'   type = type_spineplot(weights = ttnc$Freq)
+#' )
+#' 
+#' # Fancier version. Note the smart inheritance of spacing etc.
+#' tinyplot(
+#'   Survived ~ Sex | Class, facet = "by", data = ttnc,
+#'   type = type_spineplot(weights = ttnc$Freq),
+#'   palette = "Dark 2", facet.args = list(nrow = 1), axes = "t"
+#' )
+#' 
+#' # PS. If you really need to drop faceting for grouped spineplots, then at
+#' # least add alpha transparency
+#' tinyplot(
+#'   Survived ~ Sex | Class, data = ttnc,
+#'   type = type_spineplot(weights = ttnc$Freq),
+#'   alpha = 0.3
+#' )
+#' 
 #' @export
-type_spineplot = function(tol.ylab = 0.05, off = NULL, ylevels = NULL, col = NULL, xaxlabels = NULL, yaxlabels = NULL) {
+type_spineplot = function(breaks = NULL, tol.ylab = 0.05, off = NULL, ylevels = NULL, col = NULL, xaxlabels = NULL, yaxlabels = NULL, weights = NULL) {
+  col = col
   out = list(
+    data = data_spineplot(off = off, breaks = breaks, ylevels = ylevels, xaxlabels = xaxlabels, yaxlabels = yaxlabels, weights = weights),
     draw = draw_spineplot(tol.ylab = tol.ylab, off = off, col = col, xaxlabels = xaxlabels, yaxlabels = yaxlabels),
-    data = data_spineplot(off = off, ylevels = ylevels),
     name = "spineplot"
   )
   class(out) = "tinyplot_type"
   return(out)
 }
 
-#' @importFrom grDevices gray.colors
-draw_spineplot = function(tol.ylab = 0.05, off = NULL, col = NULL, xaxlabels = NULL, yaxlabels = NULL) {
-    fun = function(data_facet, ifacet, icol, ilty, ilwd, flip, facet_window_args, type_info, ...) {
-
-        ## get data subset for current facet
-        dat = data.frame(data_facet[[ifacet]])
-
-        ## handle flip internally in order to actually flip splitting direction
-        if (flip) names(dat)[c(which(names(dat) == "x"), which(names(dat) == "y"))] = c("y", "x")
-
-        ## set up frequency table
-        x = dat$x
-        x.categorical = is.factor(x)
-        breaks = type_info[["breaks"]]
-        if(!x.categorical) {
-            x = cut(as.numeric(x), breaks = breaks, include.lowest = TRUE)
-        }
-        ## TODO: process by grouping via: interaction + spacing + labeling
-        ## (for now just do interaction)
-        ## FIXME: data_facet only contains the first by group?
-        ## if (any(dat$by != "")) x = interaction(dat$by, x)
-        if(is.null(dat$weights)) {
-            tab = table(x, dat$y)
-        } else {
-            tab = as.table(tapply(dat$weights, list(x, dat$y), FUN = sum, na.rm = TRUE))
-            tab[is.na(tab)] = 0
-        }
-        nx = nrow(tab)
-        ny = ncol(tab)
-
-        ## graphical parameters
-        if (is.null(col)) col = if (icol == "#000000FF") gray.colors(ny) else seq_palette(icol, ny)
-        col = rep_len(col, ny)
-        off = if(!x.categorical) 0 else if(is.null(off)) 0.02 else off/100
-        if (is.null(yaxlabels)) yaxlabels = rev(levels(dat$y))
-
-        ## axis labels
-        yaxlabels = if(is.null(yaxlabels)) levels(y) else rep_len(yaxlabels, ny)
-        if(x.categorical) {
-            xaxlabels = if(is.null(xaxlabels)) {
-                levels(x)
-            } else {
-                rep_len(xaxlabels, nx)
-            }
-        } else {
-            xaxlabels = if(is.null(xaxlabels)) {
-                if(is.numeric(dat$x)) breaks else c(dat$x[1L], dat$x[c(diff(as.numeric(x)) > 0, TRUE)])
-            } else {
-                rep_len(xaxlabels, nx + 1L)
-            }
-        }
-
-        ## compute coordinates
-        ## cumulative proportions of x (plus off) vs. conditional cumulative proportions of y
-        yat = rbind(0, apply(proportions(tab[, ny:1L, drop = FALSE], 1L), 1L, cumsum))
-        yat[is.na(yat)] = 1
-        xat = c(0, cumsum(proportions(marginSums(tab, 1L)) + off))
-
-        ybottom = as.vector(yat[-(ny + 1L),])
-        ytop = as.vector(yat[-1L,])
-        xleft = rep(xat[1L:nx], rep(ny, nx))
-        xright = rep(xat[2L:(nx+1L)] - off, rep(ny, nx))
-
-        ## plot rectangles
-        col = rep.int(col, nx)
-        if (type_info[["xaxt"]] %in% c("l", "t", "n") &&
-            type_info[["yaxt"]] %in% c("l", "t", "n") &&
-            !all(c(type_info[["xaxt"]], type_info[["yaxt"]]) == "n")) ilwd = 0
-        if (flip) {
-            rect(ybottom, xleft, ytop, xright, col = col, lty = ilty, lwd = ilwd)
-        } else {
-            rect(xleft, ybottom, xright, ytop, col = col, lty = ilty, lwd = ilwd)
-        }
-        
-        ## axes
-        ## - standard categorical axes (xaxt/yaxt == "s") _without_ ticks
-        ## - never draw additional axis lines, box always for spinogram
-        if(type_info[["axes"]]) {
-            if (x.categorical) {
-                spine_axis(if (flip) 2 else 1, at = (xat[1L:nx] + xat[2L:(nx+1L)] - off)/2, labels = xaxlabels,
-                    type = type_info[["xaxt"]], categorical = TRUE)
-            } else {
-                spine_axis(if (flip) 2 else 1, at = xat, labels = xaxlabels,
-                    type = type_info[["xaxt"]], categorical = FALSE)
-            }
-            yat = yat[, if(flip) ncol(yat) else 1L]
-            equidist = any(diff(yat) < tol.ylab)
-            yat = if(equidist) seq.int(1/(2*ny), 1-1/(2*ny), by = 1/ny) else (yat[-1L] + yat[-length(yat)])/2
-            spine_axis(if (flip) 3 else 2, at = yat, labels = yaxlabels,
-                type = type_info[["yaxt"]], categorical = TRUE)
-            if (is_facet_position(if(flip) "bottom" else "right", ifacet, facet_window_args)) spine_axis(if (flip) 1 else 4,
-                type = type_info[["yaxt"]], categorical = FALSE)
-        }
-        if(!x.categorical && (is.null(ilwd) || ilwd > 0)) box()
-    }
-    return(fun)
-}
-
 #' @importFrom grDevices nclass.Sturges
-data_spineplot = function(off = NULL, ylevels = ylevels, ...) {
+data_spineplot = function(off = NULL, breaks = NULL, ylevels = ylevels, xaxlabels = NULL, yaxlabels = NULL, weights = NULL) {
     fun = function(
-      datapoints, breaks = NULL, weights = NULL,
-      facet = NULL, facet.args = NULL, xlim = NULL, ylim = NULL, axes = TRUE, xaxt = NULL, yaxt = NULL, ...
+      datapoints,
+      by = NULL, col = NULL, bg = NULL, palette = NULL,
+      facet = NULL, facet.args = NULL, xlim = NULL, ylim = NULL, axes = TRUE, xaxt = NULL, yaxt = NULL,
+      ...
     ) {
       
         ## process weights
@@ -152,31 +95,208 @@ data_spineplot = function(off = NULL, ylevels = ylevels, ...) {
 
         ## process y variable
         if (!is.factor(datapoints$y)) datapoints$y = factor(datapoints$y)
-	if (!is.null(ylevels)) datapoints$y = factor(y, levels = if(is.numeric(ylevels)) levels(y)[ylevels] else ylevels)
+        if (!is.null(ylevels)) datapoints$y = factor(y, levels = if(is.numeric(ylevels)) levels(y)[ylevels] else ylevels)
         if (is.null(ylim)) ylim = c(0, 1)
 
         ## adjust facet margins
         if (!is.null(facet) && is.null(facet.args[["fmar"]])) {
           facet.args[["fmar"]] = c(2, 2, 2, 2)
         }
-
-        out = list(
-            datapoints = datapoints,
-            facet.args = facet.args,
-            xlim = xlim,
-            ylim = ylim,
-            axes = FALSE,
-            frame.plot = FALSE,
-            xaxt = "n",
-            yaxt = "n",
-            xaxs = "i",
-            yaxs = "i",
-            type_info = list(breaks = breaks, axes = axes, xaxt = xaxt, yaxt = yaxt)
+        
+        x.categorical = is.factor(datapoints$x)
+        x = datapoints$x
+        y = datapoints$y
+        
+        datapoints = split(datapoints, list(datapoints$by, datapoints$facet))
+        datapoints = Filter(function(k) nrow(k) > 0, datapoints)
+        
+        # construct spineplot rectangles and breaks points for each by-facet combo
+        datapoints = Map(function(dat, x.categorical, off) {
+          ## set up frequency table
+          x = dat$x
+          if(!x.categorical) {
+            x = cut(as.numeric(x), breaks = breaks, include.lowest = TRUE)
+          }
+          ## TODO: process by grouping via: interaction + spacing + labeling
+          ## (for now just do interaction)
+          ## FIXME: data_facet only contains the first by group?
+          ## if (any(dat$by != "")) x = interaction(dat$by, x)
+          if(is.null(dat$weights)) {
+            tab = table(x, dat$y)
+          } else {
+            tab = as.table(tapply(dat$weights, list(x, dat$y), FUN = sum, na.rm = TRUE))
+            tab[is.na(tab)] = 0
+          }
+          nx = nrow(tab)
+          ny = ncol(tab)
+          
+          ## compute coordinates
+          ## cumulative proportions of x (plus off) vs. conditional cumulative proportions of y
+          yat = rbind(0, apply(proportions(tab[, ny:1L, drop = FALSE], 1L), 1L, cumsum))
+          yat[is.na(yat)] = 1
+          xat = c(0, cumsum(proportions(marginSums(tab, 1L)) + off))
+          
+          ybottom = as.vector(yat[-(ny + 1L),])
+          ytop = as.vector(yat[-1L,])
+          xleft = rep(xat[1L:nx], rep(ny, nx))
+          xright = rep(xat[2L:(nx+1L)] - off, rep(ny, nx))
+          
+          out = data.frame(
+            by = dat$by[1], # already split
+            facet = dat$facet[1], # already split
+            ymin = ybottom,
+            ymax = ytop,
+            xmin = xleft,
+            xmax = xright
+          )
+          
+          attr(out, "nx") = nx
+          attr(out, "ny") = ny
+          attr(out, "xat") = xat
+          attr(out, "yat") = yat
+          return(out)
+        }, 
+        dat = datapoints,
+        x.categorical = x.categorical,
+        off = off
         )
+        
+        nx = attr(datapoints[[1]], "nx") ## should be the same for all by/facet groups
+        ny = attr(datapoints[[1]], "ny") ## ditto
+        xat = lapply(datapoints, attr, "xat")
+        yat = lapply(datapoints, attr, "yat")
+        datapoints = do.call(rbind, datapoints)
+         
+        if (is.null(yaxlabels)) yaxlabels = rev(levels(y))
+      
+        ## axis labels
+        yaxlabels = if(is.null(yaxlabels)) levels(y) else rep_len(yaxlabels, ny)
+        if(x.categorical) {
+          xaxlabels = if(is.null(xaxlabels)) {
+            levels(x)
+          } else {
+            rep_len(xaxlabels, nx)
+          }
+        } else {
+          xaxlabels = if(is.null(xaxlabels)) {
+            if(is.numeric(x)) breaks else c(x[1L], x[c(diff(as.numeric(x)) > 0, TRUE)])
+          } else {
+            rep_len(xaxlabels, nx + 1L)
+          }
+        }
+          
+        ## grayscale flag
+        grayscale = length(unique(datapoints[["by"]])) == 1 && is.null(palette)
+        
+        out = list(
+          x = c(datapoints$xmin, datapoints$xmax), 
+          y = c(datapoints$ymin, datapoints$ymax),
+          ymin = datapoints$ymin, 
+          ymax = datapoints$ymax, 
+          xmin = datapoints$xmin, 
+          xmax = datapoints$xmax, 
+          col = col,
+          bg = bg,
+          datapoints = datapoints,
+          by = if (length(unique(datapoints$by)) == 1) by else datapoints$by, 
+          facet = if (length(unique(datapoints$facet)) == 1) facet else datapoints$facet,
+          axes = FALSE,
+          frame.plot = FALSE,
+          xaxt = "n",
+          yaxt = "n",
+          xaxs = "i",
+          yaxs = "i",
+          type_info = list(
+            off = off,
+            x.categorical = x.categorical,
+            nx = nx,
+            ny = ny,
+            xat = xat,
+            yat = yat,
+            xaxlabels = xaxlabels,
+            yaxlabels = yaxlabels,
+            breaks = breaks,
+            axes = axes,
+            xaxt = xaxt, 
+            yaxt = yaxt,
+            grayscale = grayscale
+          ),
+          facet.args = facet.args
+        )
+        
         return(out)
+        
     }
     return(fun)
 }
+
+#' @importFrom grDevices gray.colors
+draw_spineplot = function(tol.ylab = 0.05, off = NULL, col = NULL, xaxlabels = NULL, yaxlabels = NULL) {
+    fun = function(ixmin, iymin, ixmax, iymax, ilty, ilwd, icol, ibg, 
+                   flip,
+                   facet_window_args,
+                   type_info,
+                   ifacet,
+                   ...) {
+      
+      if (is.null(off)) off = type_info[["off"]]
+      if (is.null(xaxlabels)) xaxlabels = type_info[["xaxlabels"]]
+      if (is.null(yaxlabels)) yaxlabels = type_info[["yaxlabels"]]
+      xat = type_info[["xat"]][[ifacet]]
+      yat = type_info[["yat"]][[ifacet]]
+      nx = type_info[["nx"]]
+      ny = type_info[["ny"]]
+      x.categorical = type_info[["x.categorical"]]
+      grayscale = type_info[["grayscale"]]
+      
+      ## graphical parameters
+      if (is.null(col)) {
+        if (is.null(ibg)) ibg = icol
+        ibg = if (isTRUE(grayscale)) gray.colors(ny) else seq_palette(ibg, ny)
+        ibg = rep_len(ibg, ny)
+      } else {
+        ibg = col
+      }
+      
+      if (type_info[["xaxt"]] %in% c("l", "t", "n") &&
+          type_info[["yaxt"]] %in% c("l", "t", "n") &&
+          !all(c(type_info[["xaxt"]], type_info[["yaxt"]]) == "n")) ilwd = 0
+      
+      rect(
+          xleft = ixmin, ybottom = iymin, xright = ixmax, ytop = iymax,
+          lty = ilty,
+          lwd = ilwd,
+          border = par("fg"), #icol,
+          col = ibg
+      )
+      
+      ## axes
+      ## - standard categorical axes (xaxt/yaxt == "s") _without_ ticks
+      ## - never draw additional axis lines, box always for spinogram
+      if(type_info[["axes"]]) {
+          if (x.categorical) {
+              spine_axis(if (flip) 2 else 1, at = (xat[1L:nx] + xat[2L:(nx+1L)] - off)/2, labels = xaxlabels,
+                  type = type_info[["xaxt"]], categorical = TRUE)
+          } else {
+              spine_axis(if (flip) 2 else 1, at = xat, labels = xaxlabels,
+                  type = type_info[["xaxt"]], categorical = FALSE)
+          }
+          yat = yat[, if(flip) ncol(yat) else 1L]
+          equidist = any(diff(yat) < tol.ylab)
+          yat = if(equidist) seq.int(1/(2*ny), 1-1/(2*ny), by = 1/ny) else (yat[-1L] + yat[-length(yat)])/2
+          spine_axis(if (flip) 3 else 2, at = yat, labels = yaxlabels,
+              type = type_info[["yaxt"]], categorical = TRUE)
+          if (is_facet_position(if(flip) "bottom" else "right", ifacet, facet_window_args)) spine_axis(if (flip) 1 else 4,
+              type = type_info[["yaxt"]], categorical = FALSE)
+      }
+      if(!x.categorical && (is.null(ilwd) || ilwd > 0)) box()
+      
+    }
+    return(fun)
+}
+
+
+
 
 spine_axis = function(side, ..., type = "standard", categorical = TRUE) {
     type = match.arg(type, c("standard", "none", "labels", "ticks", "axis"))
@@ -210,28 +330,24 @@ spine_axis = function(side, ..., type = "standard", categorical = TRUE) {
 
 #' @importFrom grDevices col2rgb convertColor hcl
 to_hcl = function(x) {
-    x = t(col2rgb(x)/255)
+    x = t(col2rgb(x, alpha = TRUE)/255)
+    alpha = x[, 4]
+    x = x[, 1:3]
     x = convertColor(x, from = "sRGB", to = "Luv")
     x = cbind(H = atan2(x[, 3L], x[, 2L]) * 180/pi, C = sqrt(x[, 2L]^2 + x[, 3L]^2), L = x[, 1L])
     x[is.na(x[, 1L]), 1L] = 0
     x[x[, 1L] < 0, 1L] = x[x[, 1L] < 0, 1L] + 360
+    attr(x, "alpha") = alpha
     return(x)
 }
 
 seq_palette = function(x, n, power = 1.5) {
     x = drop(to_hcl(x[1L]))
-    hcl(h = x[1L],
-        c = seq.int(from = x[2L]^(1/power), to = 0, length.out = n + 1)[1L:n]^power,
-        l = 100 - seq.int(from = (100 - x[3L])^(1/power), to = pmin(8, (100 - x[3L])/2)^(1/power), length.out = n)^power)[1L:n]
+    alpha = attr(x, "alpha")
+    hcl(
+      h = x[1L],
+      c = seq.int(from = x[2L]^(1/power), to = 0, length.out = n + 1)[1L:n]^power,
+      l = 100 - seq.int(from = (100 - x[3L])^(1/power), to = pmin(8, (100 - x[3L])/2)^(1/power), length.out = n)^power,
+      alpha = alpha
+    )[1L:n]
 }
-
-## aq = transform(
-##   airquality,
-##   Month = factor(Month, labels = month.abb[unique(Month)]),
-##   Hot = Temp > median(Temp),
-##   Wind2 = cut(Wind, c(0, 5, 10, 15, 21))
-## )
-## tinyplot(Hot ~ Wind2, facet = ~ Month, data = aq, type = type_spineplot())
-## 
-## ttnc = as.data.frame(Titanic)
-## tinyplot(Survived ~ Sex, facet = ~ Class, data = ttnc, weights = ttnc$Freq, type = type_spineplot())

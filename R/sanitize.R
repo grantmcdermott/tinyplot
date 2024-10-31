8363f52b4be506b6fff42e16c524331711a3e360
Grant McDermott <grant.mcdermott@gmail.com>
2024-10-30 17:51:07 -0700

    type_spline()

--- R/sanitize.R
+++ R/sanitize.R
@@ -13,7 +13,7 @@ sanitize_type = function(type, x, y) {
 
     types = c("area", "boxplot", "density", "jitter", "ribbon", "pointrange", "hist", 
         "histogram", "errorbar", "polygon", "polypath", "rect", "segments", "points", 
-        "p", "l", "o", "b", "c", "h", "j", "s", "S", "n", "loess", "lm", "glm")
+        "p", "l", "o", "b", "c", "h", "j", "s", "S", "n", "loess", "spline", "lm", "glm")
     assert_choice(type, types, null.ok = TRUE)
 
     if (is.null(type)) {
@@ -44,6 +44,7 @@ sanitize_type = function(type, x, y) {
         "j" = type_jitter(),
         "jitter" = type_jitter(),
         "loess" = type_loess(),
+        "spline" = type_spline(),
         "glm" = type_glm(),
         "lm" = type_lm(),
         NULL  # Default case
---
+++ R/type_spline.R
@@ -0,0 +1,59 @@
+#' Spline type
+#' 
+#' @description Type function for plotting a cubic (or Hermite) spline interpolation.
+#' Arguments are passed to \code{\link[stats]{spline}}; see for default argument
+#' values.
+#' 
+#' @inheritParams stats::spline
+#' @importFrom stats spline
+#' @examples
+#' # "spline" type convenience string
+#' tinyplot(dist ~ speed, data = cars, type = "spline")
+#' 
+#' # Use `type_spline()` to pass extra arguments for customization
+#' tinyplot(dist ~ speed, data = cars, type = type_spline(method = "natural", n = 25),
+#'     add = TRUE, lty = 2)
+#' @export
+type_spline = function(
+        n = NULL,
+        method = "fmm",
+        xmin = NULL,
+        xmax = NULL,
+        xout = NULL,
+        ties = mean
+    ) {
+    out = list(
+        draw = draw_lines(),
+        data = data_spline(method = method, ties = ties, n = n, xmin = xmin, xmax = xmax, xout = xout),
+        name = "l"
+    )
+    class(out) = "tinyplot_type"
+    return(out)
+}
+
+
+data_spline = function(n, method, xmin, xmax, xout, ties, ...) {
+    fun = function(datapoints, ...) {
+        datapoints = split(datapoints, list(datapoints$facet, datapoints$by), drop = TRUE)
+        datapoints = lapply(datapoints, function(dat) {
+            if (is.null(n)) n = 3*length(dat$x)
+            if (is.null(xmax)) xmax = max(dat$x)
+            if (is.null(xmin)) xmin = min(dat$x)
+            if (is.null(xout)) {
+                fit = spline(x = dat$x, y = dat$y, n = n, method = method, xmin = xmin, xmax = xmax, ties = ties)
+            } else {
+                fit = spline(x = dat$x, y = dat$y, n = n, method = method, xmin = xmin, xmax = xmax, xout = xout, ties = ties)
+            }
+            fit = as.data.frame(fit)
+            fit$facet = dat$facet[1]
+            fit$by = dat$by[1]
+            fit
+            return(fit)
+        })
+        datapoints = do.call(rbind, datapoints)
+        out = list(datapoints = datapoints)
+        return(out)
+    }
+    return(fun)
+}
+

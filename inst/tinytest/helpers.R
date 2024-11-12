b60ec10634f13f9ba553fd2e2fb7aae6c7f0cebf
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-11-12 18:44:49 -0500

    docs

--- inst/tinytest/helpers.R
+++ inst/tinytest/helpers.R
@@ -5,6 +5,6 @@ library(tinysnapshot)
 # ON_LINUX = Sys.info()["sysname"] == "Linux"
 # if (!ON_LINUX) exit_file("Linux snapshots")
 
-# options("tinysnapshot_os" = "Linux")
+options("tinysnapshot_os" = "Linux")
 options("tinysnapshot_device" = "svglite")
 options("tinysnapshot_device_args" = list(user_fonts = fontquiver::font_families("Liberation")))

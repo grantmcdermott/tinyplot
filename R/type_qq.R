5a80814093b49657d8855d93b2d40175ce638c6f
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2024-11-13 14:33:26 -0500

    type_qq: docs + top level tinyplot() arguments

--- R/type_qq.R
+++ R/type_qq.R
@@ -6,27 +6,27 @@
 #' @param distribution Distribution function to use.
 #' @inheritParams tinyplot
 #' @examples
-#' tinyplot(~mpg, data = mtcars, type = type_qq(lty = 3))
+#' tinyplot(~mpg, data = mtcars, type = type_qq())
+#'
+#' # suppress the line
+#' tinyplot(~mpg, data = mtcars, lty = 0, type = type_qq())
 #' @importFrom stats qnorm ppoints quantile
 #' @export
-type_qq = function(distribution = qnorm, lty = NULL, col = "black", lwd = 1) {
+type_qq = function(distribution = qnorm) {
   data_qq = function(distribution) {
-    fun = function(datapoints, xlabs, ...) {
+    fun = function(datapoints, ...) {
       y = sort(datapoints$y)
       x = datapoints$x
       x = distribution(ppoints(x))
       datapoints$x = x
       datapoints$y = y
-      out = list(
-        datapoints = datapoints,
-        xlabs = xlabs
-      )
+      out = list(datapoints = datapoints)
       return(out)
     }
   }
 
-  draw_qq = function(lty, col, lwd) {
-    fun = function(ix, iy, icol, ibg, ipch, ilwd, cex, xlab, ...) {
+  draw_qq = function() {
+    fun = function(ix, iy, icol, ibg, ipch, ilwd, ilty, cex, xlab, ...) {
       points(
         x = ix,
         y = iy,
@@ -38,21 +38,21 @@ type_qq = function(distribution = qnorm, lty = NULL, col = "black", lwd = 1) {
         cex = cex
       )
 
-      if (!is.null(lty)) {
+      if (!is.null(ilty)) {
         iy <- quantile(iy, c(0.25, 0.75))
         ix <- quantile(ix, c(0.25, 0.75))
         slope <- diff(iy) / diff(ix)
         intercept <- iy[1] - slope * ix[1]
-        abline(a = intercept, b = slope, lty = lty, col = col, lwd = lwd)
+        abline(a = intercept, b = slope, lty = ilty, col = icol, lwd = ilwd)
       }
     }
     return(fun)
   }
 
   out = list(
-    draw = draw_qq(lty = lty, col = col, lwd = lwd),
+    draw = draw_qq(),
     data = data_qq(distribution = distribution),
-    name = "p"
+    name = "qq"
   )
   class(out) = "tinyplot_type"
   return(out)

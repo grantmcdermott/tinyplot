83e14b4b15090c0dd3d6d66c355061812c03da27
Grant McDermott <grant.mcdermott@gmail.com>
2024-11-22 15:30:15 -0800

    gradient tests

--- inst/tinytest/test-ridge.R
+++ inst/tinytest/test-ridge.R
@@ -13,3 +13,33 @@ f = function() {
     bg = "light blue", col = "black")
 }
 expect_snapshot_plot(f, label = "ridge_02")
+
+f = function() {
+  tinyplot(Species ~ Sepal.Width, data = iris, type = type_ridge(gradient = TRUE))
+}
+expect_snapshot_plot(f, label = "ridge_gradient")
+
+f = function() {
+  tinyplot(
+    Species ~ Sepal.Width, data = iris,
+    type = type_ridge(gradient = TRUE, breaks = seq(2, 4.5, by = 0.5))
+  )
+}
+expect_snapshot_plot(f, label = "ridge_gradient_discrete")
+
+f = function() {
+  tinyplot(
+    Species ~ Sepal.Width, data = iris,
+    type = type_ridge(
+      gradient = hcl.colors(250, "Dark Mint")[c(250:1, 1:250)],
+      probs = 0:500/500
+    )
+  )
+}
+expect_snapshot_plot(f, label = "ridge_gradient_probs")
+
+tinyplot(
+  am ~ mpg, facet = ~vs, data = mtcars, type = type_ridge(gradient = "agsunset"),
+  col = "white"
+)
+expect_snapshot_plot(f, label = "ridge_gradient_facet")

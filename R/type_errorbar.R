a5ad43e99c54df283693697989183221dd21cfda
Grant McDermott <grant.mcdermott@gmail.com>
2025-09-07 12:16:18 -0700

    more explict doc

--- R/type_errorbar.R
+++ R/type_errorbar.R
@@ -2,7 +2,10 @@
 #'
 #' @description Type function(s) for producing error bar and pointrange plots.
 #'
-#' @param dodge Numeric value (>= 0) for dodging of overlapping points. Default is 0 (no dodging).
+#' @param dodge Numeric value (>= 0) for dodging of overlapping points. Dodging
+#'   is scaled relative to the x-axis tick locations (i.e., unique levels of
+#'   `x`). For example, `dodge = 0.5` would place the outermost dodged points
+#'   exactly midway the between axis ticks. Default value is 0 (no dodging).
 #' @inheritParams graphics::arrows
 #' @examples
 #' mod = lm(mpg ~ wt * factor(am), mtcars)
@@ -54,7 +57,20 @@ type_errorbar = function(length = 0.05, dodge = 0) {
 
 
 draw_errorbar = function(length = 0.05) {
-    fun = function(ix, iy, ixmin, iymin, ixmax, iymax, icol, ibg, ipch, ilwd, icex, ...) {
+    fun = function(
+        ix,
+        iy,
+        ixmin,
+        iymin,
+        ixmax,
+        iymax,
+        icol,
+        ibg,
+        ipch,
+        ilwd,
+        icex,
+        ...
+    ) {
         arrows(
             x0 = ixmin,
             y0 = iymin,
@@ -66,7 +82,15 @@ draw_errorbar = function(length = 0.05) {
             angle = 90,
             code = 3
         )
-        draw_points()(ix = ix, iy = iy, icol = icol, ibg = ibg, ipch = ipch, ilwd = ilwd, icex = icex)
+        draw_points()(
+            ix = ix,
+            iy = iy,
+            icol = icol,
+            ibg = ibg,
+            ipch = ipch,
+            ilwd = ilwd,
+            icex = icex
+        )
     }
     return(fun)
 }

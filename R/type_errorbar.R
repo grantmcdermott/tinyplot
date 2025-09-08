b74f0175f493da6bb2f34eb50533137d303c365d
Vincent Arel-Bundock <vincentarelbundock@users.noreply.github.com>
2025-09-07 21:16:00 -0400

    `type_pointrange()` and `type_errorange()` dodge (#461)
    * issue #406 type_pointrange()/errorbar() get a `dodge` argument
    
    * simplification + shiftwidth=2
    
    * man
    
    * test pointrange dodge
    
    * more explict doc
    
    * fixed.pos
    
    ---------
    
    Co-authored-by: Grant McDermott <grant.mcdermott@gmail.com>

--- R/type_errorbar.R
+++ R/type_errorbar.R
@@ -1,38 +1,94 @@
 #' Error bar and pointrange plot types
-#' 
+#'
 #' @description Type function(s) for producing error bar and pointrange plots.
-#' 
+#'
+#' @param dodge Numeric value (>= 0) for dodging of overlapping `by` groups.
+#'   Dodging is scaled relative to the x-axis tick locations (i.e., unique
+#'   levels of `x`). For example, `dodge = 0.5` would place the outermost dodged
+#'   groups exactly midway the between axis ticks. Default value is 0 (no
+#'   dodging).
+#' @param fixed.pos Logical indicating whether dodged groups should retain a
+#'   fixed relative position based on their group value. Relevant for `x`
+#'   categories that only have a subset of the total number of groups. Defaults
+#'   to `FALSE`, in which case dodging is based on the number of unique groups
+#'   present in that `x` category alone. See Examples.
 #' @inheritParams graphics::arrows
 #' @examples
 #' mod = lm(mpg ~ wt * factor(am), mtcars)
 #' coefs = data.frame(names(coef(mod)), coef(mod), confint(mod))
 #' colnames(coefs) = c("term", "est", "lwr", "upr")
-#' 
+#'
 #' op = tpar(pch = 19)
-#'  
+#'
 #' # "errorbar" and "pointrange" type convenience strings
 #' tinyplot(est ~ term, ymin = lwr, ymax = upr, data = coefs, type = "errorbar")
 #' tinyplot(est ~ term, ymin = lwr, ymax = upr, data = coefs, type = "pointrange")
-#' 
+#'
 #' # Use `type_errorbar()` to pass extra arguments for customization
-#' tinyplot(est ~ term, ymin = lwr, ymax = upr, data = coefs, type = type_errorbar(length = 0.2))
-#' 
+#' tinyplot(est ~ term, ymin = lwr, ymax = upr, data = coefs,
+#'          type = type_errorbar(length = 0.2))
+#'
+#' # display three models side-by-side with dodging
+#'
+#' models = list(
+#'     "Model A" = lm(mpg ~ wt + cyl, data = mtcars),
+#'     "Model B" = lm(mpg ~ wt + hp + cyl, data = mtcars),
+#'     "Model C" = lm(mpg ~ wt, data = mtcars)
+#' )
+#'
+#' results = lapply(names(models), function(m) {
+#'     data.frame(
+#'         model = m,
+#'         term = names(coef(models[[m]])),
+#'         estimate = coef(models[[m]]),
+#'         setNames(data.frame(confint(models[[m]])), c("conf.low", "conf.high"))
+#'     )
+#' })
+#' results = do.call(rbind, results)
+#'
+#' tinyplot(estimate ~ term | model,
+#'          ymin = conf.low, ymax = conf.high,
+#'          data = results,
+#'          type = type_pointrange(dodge = 0.2))
+#'
+#' # Note that the default dodged position is based solely on the number of
+#' # groups (here: models) available to each coefficient term. To fix the
+#' # position consistently across all terms, use `fixed.pos = TRUE`.
+#'
+#' tinyplot(estimate ~ term | model,
+#'          ymin = conf.low, ymax = conf.high,
+#'          data = results,
+#'          type = type_pointrange(dodge = 0.2, fixed.pos = TRUE))
+#'
 #' tpar(op)
-#' 
+#'
 #' @export
-type_errorbar = function(length = 0.05) {
-  out = list(
-    draw = draw_errorbar(length = length),
-    data = data_pointrange(),
-    name = "p"
-  )
-  class(out) = "tinyplot_type"
-  return(out)
+type_errorbar = function(length = 0.05, dodge = 0, fixed.pos = FALSE) {
+    out = list(
+        draw = draw_errorbar(length = length),
+        data = data_pointrange(dodge = dodge, fixed.pos = fixed.pos),
+        name = "p"
+    )
+    class(out) = "tinyplot_type"
+    return(out)
 }
 
 
 draw_errorbar = function(length = 0.05) {
-    fun = function(ix, iy, ixmin, iymin, ixmax, iymax, icol, ibg, ipch, ilwd, icex, ...) {
+    fun = function(
+        ix,
+        iy,
+        ixmin,
+        iymin,
+        ixmax,
+        iymax,
+        icol,
+        ibg,
+        ipch,
+        ilwd,
+        icex,
+        ...
+    ) {
         arrows(
             x0 = ixmin,
             y0 = iymin,
@@ -44,9 +100,15 @@ draw_errorbar = function(length = 0.05) {
             angle = 90,
             code = 3
         )
-        draw_points()(ix = ix, iy = iy, icol = icol, ibg = ibg, ipch = ipch, ilwd = ilwd, icex = icex)
+        draw_points()(
+            ix = ix,
+            iy = iy,
+            icol = icol,
+            ibg = ibg,
+            ipch = ipch,
+            ilwd = ilwd,
+            icex = icex
+        )
     }
     return(fun)
 }
-
-
--- R/type_pointrange.R
+++ R/type_pointrange.R
@@ -1,9 +1,12 @@
 #' @rdname type_errorbar
 #' @export
-type_pointrange = function() {
+type_pointrange = function(dodge = 0, fixed.pos = FALSE) {
+  assert_numeric(dodge, len = 1, lower = 0)
+  assert_logical(fixed.pos)
+
   out = list(
     draw = draw_pointrange(),
-    data = data_pointrange(),
+    data = data_pointrange(dodge = dodge, fixed.pos = fixed.pos),
     name = "p"
   )
   class(out) = "tinyplot_type"
@@ -11,42 +14,90 @@ type_pointrange = function() {
 }
 
 
-
 draw_pointrange = function() {
-    fun = function(ix, iy, ixmin, iymin, ixmax, iymax, icol, ibg, ipch, ilwd, icex, ...) {
-        segments(
-            x0 = ixmin,
-            y0 = iymin,
-            x1 = ixmax,
-            y1 = iymax,
-            col = icol,
-            lwd = ilwd
-        )
-        draw_points()(ix = ix, iy = iy, icol = icol, ibg = ibg, ipch = ipch, ilwd = ilwd, icex = icex)
-    }
-    return(fun)
+  fun = function(
+    ix,
+    iy,
+    ixmin,
+    iymin,
+    ixmax,
+    iymax,
+    icol,
+    ibg,
+    ipch,
+    ilwd,
+    icex,
+    ...
+  ) {
+    segments(
+      x0 = ixmin,
+      y0 = iymin,
+      x1 = ixmax,
+      y1 = iymax,
+      col = icol,
+      lwd = ilwd
+    )
+    draw_points()(
+      ix = ix,
+      iy = iy,
+      icol = icol,
+      ibg = ibg,
+      ipch = ipch,
+      ilwd = ilwd,
+      icex = icex
+    )
+  }
+  return(fun)
 }
 
 
-data_pointrange = function() {
-    fun = function(datapoints, xlabs, ...) {
-        if (is.character(datapoints$x)) datapoints$x = as.factor(datapoints$x)
-        if (is.factor(datapoints$x)) {
-            ## original data (i.e., no new sorting by factor)
-            xlvls = unique(datapoints$x)
-            datapoints$x = factor(datapoints$x, levels = xlvls)
-            xlabs = seq_along(xlvls)
-            names(xlabs) = xlvls
-            datapoints$x = as.integer(datapoints$x)
-        }
-        datapoints$xmin = datapoints$x
-        datapoints$xmax = datapoints$x
-        out = list(
-            x = datapoints$x,
-            xlabs = xlabs,
-            datapoints = datapoints)
-
-        return(out)
+data_pointrange = function(dodge, fixed.pos) {
+  fun = function(datapoints, xlabs, ...) {
+    if (is.character(datapoints$x)) {
+      datapoints$x = as.factor(datapoints$x)
     }
-    return(fun)
+    if (is.factor(datapoints$x)) {
+      ## original data (i.e., no new sorting by factor)
+      xlvls = unique(datapoints$x)
+      datapoints$x = factor(datapoints$x, levels = xlvls)
+      xlabs = seq_along(xlvls)
+      names(xlabs) = xlvls
+      datapoints$x = as.integer(datapoints$x)
+    }
+    datapoints$xmin = datapoints$x
+    datapoints$xmax = datapoints$x
+
+    # dodge
+    if (dodge != 0) {
+      if (fixed.pos) {
+        n = nlevels(datapoints$by)
+        d = cumsum(rep(dodge, n))
+        d = d - mean(d)
+        x_adj = d[as.integer(datapoints$by)]
+        datapoints$x = datapoints$x + x_adj
+        datapoints$xmin = datapoints$xmin + x_adj
+        datapoints$xmax = datapoints$xmax + x_adj
+      } else {
+        xuniq = unique(datapoints$x)
+        for (i in seq_along(xuniq)) {
+          idx = which(datapoints$x == xuniq[i])
+          n = length(idx)
+          d = cumsum(rep(dodge, n))
+          d = d - mean(d)
+          datapoints$x[idx] = datapoints$x[idx] + d
+          datapoints$xmin[idx] = datapoints$xmin[idx] + d
+          datapoints$xmax[idx] = datapoints$xmax[idx] + d
+        }
+      }
+    }
+
+    out = list(
+      x = datapoints$x,
+      xlabs = xlabs,
+      datapoints = datapoints
+    )
+
+    return(out)
+  }
+  return(fun)
 }

ef588b88eb3daebe730a284fa5e402437d8a3ffa
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2025-09-08 23:31:13 -0400

    fix draw_title() signature

--- R/tinyplot.R
+++ R/tinyplot.R
@@ -891,8 +891,6 @@ tinyplot.default = function(
       datapoints = swap_columns(datapoints, "xmax", "ymax")
     }
   }
-  
-
 
 
   #############
@@ -937,9 +935,9 @@ tinyplot.default = function(
   list2env(fargs, environment())
 
 
-  ################################################
-  # split and process data and aesthetics by group
-  ################################################
+  #####################
+  # aesthetics by group
+  #####################
 
   by_ordered = FALSE
   by_continuous = !null_by && inherits(datapoints$by, c("numeric", "integer"))
@@ -949,9 +947,7 @@ tinyplot.default = function(
   } else if (!null_by) {
     by_ordered = is.ordered(by)
   }
-
   
-  # aesthetics by group: col, bg, etc.
   ngrps = if (null_by) 1L else if (is.factor(by)) nlevels(by) else if (by_continuous) 100L else length(unique(by))
   pch = by_pch(ngrps = ngrps, type = type, pch = pch)
   lty = by_lty(ngrps = ngrps, type = type, lty = lty)
@@ -989,10 +985,11 @@ tinyplot.default = function(
   }
   
 
-  # Determine the number and arrangement of facets.
-  # Note: We're do this up front, so we can make some adjustments to legend cex
-  #   next (if there are facets). But the actual drawing of the facets will only
-  #   come later.
+  ###############
+  # facets: count
+  ###############
+
+  # before legend becase it requires `cex_fct_adj`
   if (length(unique(datapoints$facet)) == 1) {
     datapoints[["facet"]] = NULL
   }
@@ -1002,13 +999,11 @@ tinyplot.default = function(
   list2env(fargs, environment())
 
 
-  #
-  ## Global plot elements (legend and titles)
-  #
-
-  # place and draw the legend
+  #########
+  # legends
+  #########
   
-   # simple indicator variables for later use
+  # simple indicator variables for later use
   has_legend = FALSE
   dual_legend = bubble && !null_by && !isFALSE(legend)
   lgnd_cex = NULL
@@ -1144,82 +1139,18 @@ tinyplot.default = function(
     plot.new()
   }
 
-  # Titles. Only draw these if add = FALSE
+
+  ####################
+  # title and subtitle
+  ####################
   if (!add) {
-    # main title
-    # Note that we include a special catch for the main title if legend is
-    # "top!" (and main is specified in the first place).
-    legend_eval = tryCatch(eval(legend), error = function(e) NULL)
-    # Extra bit of footwork if user passed legend = legend(...) instead of
-    # legend = list(...), since the call environment is tricky
-    if (is.null(legend_eval)) {
-      legend_eval = tryCatch(paste0(legend)[[2]], error = function(e) NULL)
-    }
-
-    adj_title = !is.null(legend) && (legend == "top!" || (!is.null(legend_args[["x"]]) && legend_args[["x"]] == "top!") || (is.list(legend_eval) && legend_eval[[1]] == "top!"))
-
-    # For the "top!" legend case, bump main title up to make space for the
-    # legend beneath it: Take the normal main title line gap (i.e., 1.7 lines)
-    # and add the difference between original top margin and new one (i.e.,
-    # which should equal the height of the new legend). Note that we also
-    # include a 0.1 epsilon bump, which we're using to reset the tinyplot
-    # window in case of recursive "top!" calls. (See draw_legend code.)
-
-    if (isTRUE(adj_title)) {
-      line_main = par("mar")[3] - opar[["mar"]][3] + 1.7 + 0.1
-    } else {
-      line_main = NULL
-    }
-
-    if (!is.null(sub)) {
-      if (isTRUE(get_tpar("side.sub", 1) == 3)) {
-        if (is.null(line_main)) line_main = par("mgp")[3] + 1.7 - .1
-        line_main = line_main + 1.2
-      }
-      if (isTRUE(get_tpar("side.sub", 1) == 3)) {
-        line_sub = get_tpar("line.sub", 1.7)
-      } else {
-        line_sub = get_tpar("line.sub", 4)
-      }
-      args = list(
-        text = sub,
-        line = line_sub,
-        cex = get_tpar("cex.sub", 1.2),
-        col = get_tpar("col.sub", "black"),
-        adj = get_tpar(c("adj.sub", "adj")),
-        font = get_tpar("font.sub", 1),
-        side = get_tpar("side.sub", 1),
-        las = 1
-      )
-      args = Filter(function(x) !is.null(x), args)
-      do.call(mtext, args)
-    }
-
-    if (!is.null(main)) {
-      args = list(
-        main = main,
-        line = line_main,
-        cex.main = get_tpar("cex.main", 1.4),
-        col.main = get_tpar("col.main", "black"),
-        font.main = get_tpar("font.main", 2),
-        adj = get_tpar(c("adj.main", "adj"), 3))
-      args = Filter(function(x) !is.null(x), args)
-      do.call(title, args)
-    }
-
-
-    # Axis titles
-    args = list(xlab = xlab)
-    args[["adj"]] = get_tpar(c("adj.xlab", "adj"))
-    do.call(title, args)
-    args = list(ylab = ylab)
-    args[["adj"]] = get_tpar(c("adj.ylab", "adj"))
-    do.call(title, args)
+    draw_title(main, sub, xlab, ylab, legend, legend_args, opar)
   }
 
-  #
-  ## Exterior plot elements (plot and facet windows, axes, etc.)
-  #
+
+  ##############
+  # facets: draw
+  ##############
 
   omar = NULL # Placeholder variable for now, which we re-assign as part of facet margins
 
@@ -1315,9 +1246,9 @@ tinyplot.default = function(
   list2env(facet_window_args, environment())
 
 
-  #
-  ## Interior plot elements
-  #
+  ###########################
+  # split and draw datapoints
+  ###########################
 
   # Finally, we can draw all of the plot elements (points, lines, etc.)
   # We'll do this via a nested loops:
@@ -1459,8 +1390,11 @@ tinyplot.default = function(
     }
   }
   
+
+  #########################################
+  # save end pars for possible recall later
+  #########################################
   if (!add) {
-    # save end pars for possible recall later
     recordGraphics(
       {
         apar = par(no.readonly = TRUE)

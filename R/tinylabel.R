5f846d3c29769a49130293649f9883c081dafd58
Grant McDermott <grant.mcdermott@gmail.com>
2025-09-09 21:51:31 -0700

    tinylabel numeric fixes (#468)
    * issue #449
    
    * issue #467
    
    * news

--- R/tinylabel.R
+++ R/tinylabel.R
@@ -91,9 +91,17 @@ labeller_fun = function(label = "percent") {
   abs_ = substr(label, 1L, 4L) == "abs_"
   if (abs_) label = substr(label, 5L, nchar(label))
 
-  ## actual formatting function
+  ## actual formatting functions
+  
   format_percent = function(x) {
-    sprintf("%.0f%%", x * 100)
+    max_decimals = 5L
+    pct = as.numeric(x) * 100
+    d = Find(
+      function(d) length(
+        unique(sprintf(paste0('%.', d, 'f%%'), pct))) == length(pct),
+        0:max_decimals
+    ) %||% max_decimals
+    sprintf(paste0('%.', d, 'f%%'), pct)
   }
   
   format_comma = function(x) {
@@ -113,6 +121,7 @@ labeller_fun = function(label = "percent") {
   }
   
   format_log = function(x) {
+    x = as.numeric(x)
     parse(text = paste0(10, "^", format(log10(x), digits = 3)))
   }
   

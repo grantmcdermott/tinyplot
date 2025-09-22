a6674d947b40ab2eed7b79970e988238b0cf7168
Grant McDermott <gmcd@amazon.com>
2025-09-22 16:53:21 -0700

    tweak

--- vignettes/gallery_figs/boxplot-by-penguins.R
+++ vignettes/gallery_figs/boxplot-by-penguins.R
@@ -4,5 +4,6 @@ plt(
   body_mass ~ species | sex, data = penguins,
   type = type_boxplot(boxwex = 0.6, staplewex = 0, outline = FALSE),
   lty = 1,
-  flip = TRUE, yaxl = ","
+  flip = TRUE, yaxl = ",",
+  grid = TRUE
 )

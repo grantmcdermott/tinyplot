49ad60f2218d51646e444ca6558829df79ea538d
Grant McDermott <gmcd@amazon.com>
2025-09-27 14:20:23 -0700

    auto-inherit y values if labels NULL

--- R/type_text.R
+++ R/type_text.R
@@ -3,8 +3,9 @@
 #' @description Type function for adding text annotations to a plot at the
 #' specified (`x`,`y`) coordinates.
 #' 
-#' @param labels Character vector of length 1 or of the same length as the
-#'  number of x,y coordinates.
+#' @param labels Character vector of length `1` or the same length as the
+#'   number of `x`,`y` coordinates. If left as `NULL`, then the labels will
+#'   automatically inherit the corresponding `y` values.
 #' @param family The name of a font family. Default of `NULL` means that the
 #' family will be the same as the main plot text, following
 #' \code{\link[graphics]{par}}. Note that if a `family` argument is provided,
@@ -40,7 +41,7 @@
 #' )
 #'
 #' @export
-type_text = function(labels, adj = NULL, pos = NULL, offset = 0.5, family = NULL, font = NULL, vfont = NULL, xpd = NULL, srt = 0, clim = c(0.5, 2.5)) {
+type_text = function(labels = NULL, adj = NULL, pos = NULL, offset = 0.5, family = NULL, font = NULL, vfont = NULL, xpd = NULL, srt = 0, clim = c(0.5, 2.5)) {
   out = list(
     draw = draw_text(adj = adj, pos = pos, offset = offset, vfont = vfont, family = family, font = font, xpd = xpd, srt = srt),
     data = data_text(labels = labels, clim = clim),
@@ -50,8 +51,9 @@ type_text = function(labels, adj = NULL, pos = NULL, offset = 0.5, family = NULL
   return(out)
 }
 
-data_text = function(labels, clim = c(0.5, 2.5)) {
+data_text = function(labels = NULL, clim = c(0.5, 2.5)) {
   fun = function(datapoints, legend_args, cex = NULL, ...) {
+    if (is.null(labels)) labels = datapoints$y
     if (length(labels) != 1 && length(labels) != nrow(datapoints)) {
       msg = sprintf("`labels` must be of length 1 or %s.", nrow(datapoints))
       stop(msg, call. = FALSE)
@@ -60,7 +62,6 @@ data_text = function(labels, clim = c(0.5, 2.5)) {
     if (is.factor(datapoints$x)) datapoints$x = as.numeric(datapoints$x)
     if (is.factor(datapoints$y)) datapoints$y = as.numeric(datapoints$y)
 
-    # browser()
     bubble = FALSE
     bubble_cex = 1
     if (!is.null(cex) && length(cex) == nrow(datapoints)) {

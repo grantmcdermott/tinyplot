4c005de1014978d84f8e13a9a8c0dab4fbca7d1b
Grant McDermott <gmcd@amazon.com>
2025-11-25 12:45:59 -0800

    support dodge arg in points and lines types

--- R/type_lines.R
+++ R/type_lines.R
@@ -3,6 +3,7 @@
 #' @description Type function for plotting lines.
 #' 
 #' @inheritParams graphics::plot.default
+#' @inheritParams dodge_positions
 #' 
 #' @examples
 #' # "l" type convenience character string
@@ -12,14 +13,46 @@
 #' tinyplot(circumference ~ age | Tree, data = Orange, type = type_lines(type = "s"))
 #' 
 #' @export
-type_lines = function(type = "l") {
+type_lines = function(type = "l", dodge = 0, fixed.dodge = FALSE) {
   out = list(
     draw = draw_lines(type = type),
-    data = NULL,
+    data = data_lines(dodge = dodge, fixed.dodge = fixed.dodge),
     name = type
   )
   class(out) = "tinyplot_type"
   return(out)
+}
+
+
+data_lines = function(dodge = 0, fixed.dodge = FALSE) {
+  if (is.null(dodge) || dodge == 0) return(NULL)
+  fun = function(settings, ...) {
+    env2env(settings, environment(), c("datapoints", "xlabs"))
+
+    if (is.character(datapoints$x)) {
+      datapoints$x = as.factor(datapoints$x)
+    }
+    if (is.factor(datapoints$x)) {
+      xlvls = unique(datapoints$x)
+      datapoints$x = factor(datapoints$x, levels = xlvls)
+      xlabs = seq_along(xlvls)
+      names(xlabs) = xlvls
+      datapoints$x = as.integer(datapoints$x)
+    }
+
+    # dodge
+    if (dodge != 0) {
+      datapoints = dodge_positions(datapoints, dodge, fixed.dodge)
+    }
+
+    x = datapoints$x
+    env2env(environment(), settings, c(
+      "x",
+      "xlabs",
+      "datapoints"
+    ))
+  }
+  fun
 }
 
 

d7bd0a43bef5c3f38b53d2c9edcba2728b6c8658
Brock <kbrock@ab.mpg.de>
2026-04-22 10:30:13 +0200

    Better, but maybe by chance

--- R/facet.R
+++ R/facet.R
@@ -230,6 +230,10 @@ draw_facet_window = function(
       mfgj = ii %% nfacet_cols
       if (mfgj == 0) mfgj = nfacet_cols
       par(mfg = c(mfgi, mfgj))
+      if (ii == 1) {
+        xin = grconvertX(1, "npc", "inches")
+        yin = grconvertY(1, "npc", "inches")
+      }
     }
 
     ## Set the plot window
@@ -311,7 +315,7 @@ draw_facet_window = function(
         # individual facet.
         xfree = if (!is.null(facet)) split(c(x, xmin, xmax), facet)[[ii]] else c(x, xmin, xmax)
         yfree = if (!is.null(facet)) split(c(y, ymin, ymax), facet)[[ii]] else c(y, ymin, ymax)
-        lim = calc_lim(range(xfree, na.rm = TRUE), range(yfree, na.rm = TRUE), asp)
+        lim = calc_lim(range(xfree, na.rm = TRUE), range(yfree, na.rm = TRUE), asp, xin, yin)
         if (null_xlim) xlim = lim$xlim
         if (null_ylim) ylim = lim$ylim
         xext = extendrange(xlim, f = 0.04)

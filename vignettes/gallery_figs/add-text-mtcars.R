88b78d3b7ebd8a7ca0fd43d5323f1a96ff826c0d
Grant McDermott <gmcd@amazon.com>
2026-06-16 18:09:59 -0700

    text tweak

--- vignettes/gallery_figs/add-text-mtcars.R
+++ vignettes/gallery_figs/add-text-mtcars.R
@@ -3,12 +3,15 @@ library("tinyplot")
 plt(
   mpg ~ hp | factor(cyl),
   data = mtcars,
-  legend = list("topright!", bty = "o"),
-  theme =  "classic"
+  legend = list("topright!", bty = "o", title = "Cyclinders"),
+  xlab = "Gross horsepower",
+  ylab = "Miles per gallon",
+  theme = "classic"
 )
 plt_add(type = type_text(
   labels = row.names(mtcars),
   adj = -0.05,
   srt = 15,
-  xpd = NA
+  xpd = NA,
+  repel = TRUE
 ))

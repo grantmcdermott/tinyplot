720bdcbc254e851477b1d8574a9e2223d1d5824b
Grant McDermott <gmcd@amazon.com>
2026-06-16 17:59:46 -0700

    use float theme

--- vignettes/gallery_figs/points-iris-better.R
+++ vignettes/gallery_figs/points-iris-better.R
@@ -3,9 +3,6 @@ library(tinyplot)
 tinyplot(
   Sepal.Length ~ Petal.Length | Species,
   data = iris,
-  palette = "dark",
-  pch = 16,
-  grid = TRUE,
-  frame = FALSE,
+  theme = 'float', grid = TRUE,
   main = "A more exciting scatterplot"
 )

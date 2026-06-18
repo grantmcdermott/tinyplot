1d019a957c40a52737ab7d0c0d1648ab5411b009
Grant McDermott <gmcd@amazon.com>
2026-06-17 18:10:38 -0700

    update text plot

--- vignettes/gallery_figs/add-text-mtcars.R
+++ vignettes/gallery_figs/add-text-mtcars.R
@@ -1,17 +1,40 @@
 library("tinyplot")
 
-plt(
-  mpg ~ hp | factor(cyl),
-  data = mtcars,
-  legend = list("topright!", bty = "o", title = "Cyclinders"),
-  xlab = "Gross horsepower",
-  ylab = "Miles per gallon",
-  theme = "classic"
+mtcars2 = within(
+  mtcars, {
+    make = sub(" .*", "", row.names(mtcars))
+    model = sub("^\\S+\\s+", "", row.names(mtcars))
+    merc = factor(ifelse(make == "Merc", "Mercedes", "Other"), levels = c("Other", "Mercedes"))
+  }
 )
-plt_add(type = type_text(
-  labels = row.names(mtcars),
-  adj = -0.05,
-  srt = 15,
-  xpd = NA,
-  repel = TRUE
-))
+
+plt(
+  qsec ~ mpg | merc,
+  data = mtcars2,
+  legend = list(title = NULL),
+  ylab = "1/4 mile time (s)",
+  xlab = "Miles per gallon",
+  main = "Performance vs efficiency",
+  sub = "Mercedes vs the rest",
+  cap = "Notes: Model numbers highlighted in text",
+  theme = list("web", palette.qualitative = c("#0FCFC0", "#F79CD4"))
+)
+plt_add(
+  type = "text", labels = mtcars2$model, repel = TRUE, xpd = NA, pos = 4,
+  alpha = 0.4
+)
+plt_add(
+  data = subset(mtcars2, make=="Merc")
+)
+plt_add(
+  data = subset(mtcars2, make=="Merc"),
+  type = "text", labels = subset(mtcars2, make == "Merc")$model,
+  repel = TRUE, xpd = NA, pos = 4, font = 2
+)
+
+# optional annotations
+arrows(12, 19, 12, 23, length = 0.1, angle = 20, col = "darkgrey", xpd = NA)
+text(12, mean(c(19, 23)), "slower", srt = 90, adj = c(0.5, 1.5), col = "darkgrey", font = 3)
+arrows(24, 15, 34.5, 15, length = 0.1, angle = 20, col = "darkgrey", xpd = NA)
+text(mean(c(24, 34.5)), 15, "thriftier", adj = c(0.5, -1), col = "darkgrey", font = 3)
+

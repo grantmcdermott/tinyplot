8cf5a6a9e5866c9db7c8e33d442d8906926577b3
Grant McDermott <gmcd@amazon.com>
2026-06-18 08:58:27 -0700

    better simpson's paradox figure

--- vignettes/gallery_figs/simpsons-paradox.R
+++ vignettes/gallery_figs/simpsons-paradox.R
@@ -1,17 +1,14 @@
 library(tinyplot)
 
-op = par(pch = 15)
 plt(
   bill_dep ~ bill_len | species, data = penguins,
-  pch = "by",
-  cex = 1.2, alpha = 0.5,
-  xlab = "Bill length (mm)",
-  ylab = "Bill depth (mm)",
-  main = "Simpson's paradox",
-  sub = "Penguin bill dimensions",
-  cap = "Notes: 1) Front colour ribbons = group-wise linear fit.\n2) Background grey ribbon = pooled linear fit.",
-  theme = "clean2",
-  draw = plt_add(bill_dep ~ bill_len, type = "lm", col = "gray50")
+  pch = c(21, 24, 22), fill = 0.6, cex = 1.2,
+  main = "Bill dimensions for different penguin species",
+  sub = "Illustration of Simpson's paradox",
+  cap = "Notes: Lines show OLS fit for species-specific (solid) vs. pooled (dotted grey) samples.",
+  xlab = "Bill length (mm)", ylab = "Bill depth (mm)",
+  legend = list("bottomright", title = "Species", bty = "o", lty = 1),
+  theme = list("minimal", palette.qualitative = c("darkorange", "purple", "cyan4"))
 )
-plt_add(type = "lm")
-par(op)
+plt_add(type = type_lm(se = FALSE), lwd = 2)
+plt_add(bill_dep ~ bill_len, type = type_lm(se = FALSE), lwd = 2, lty = 2, col = "darkgray")

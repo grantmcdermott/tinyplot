148ad654ab110af17f12b7973a2ac1acc257c5ca
Grant McDermott <gmcd@amazon.com>
2026-06-18 10:32:27 -0700

    gallery tweaks

--- vignettes/gallery_figs/bubble-quakes.R
+++ vignettes/gallery_figs/bubble-quakes.R
@@ -4,22 +4,19 @@ magnitude = quakes$mag
 plt(
   lat ~ long | depth, data = quakes,
   cex = magnitude, clim = c(0.5, 4.5),
-  col = "#3D3532",
-  fill = 0.7, pch = 21,
+  pch = 21, col = "#3D3532", fill = 0.7,
+  xlab = "Longitutde", ylab = "Latitude",
   main = "Earthquakes off Fiji",
-  xlab = "Longitutde",
-  ylab = "Latitude",
   sub = "Data courtesy of the Harvard PRIM-H project",
-  palette = "sunsetdark",
   # custom ephemeral theme
   theme = list(
     "dynamic",
     bty = "n",
     bg = "#E0D9D2",
     cex = 1.2, cex.main = 1.5, cex.lab = 1.2,
-    col = "#3D3532",
     family = "HersheyScript",
     grid = TRUE, grid.col = "gray70",
-    lwd = 0.5
+    lwd = 0.5,
+    palette.sequential = "sunsetdark"
   )
 )

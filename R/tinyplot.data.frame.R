145c6455940d17e767779ece0f66d12469cece55
Achim Zeileis <Achim.Zeileis@R-project.org>
2026-06-18 03:15:36 +0200

    pairs-esque display in data.frame method for 3 or more variables

--- R/tinyplot.data.frame.R
+++ R/tinyplot.data.frame.R
@@ -7,10 +7,8 @@
 #'   or without a formula. The case with the formula mainly facilitates
 #'   using `tinyplot()` in combination with pipes. The case without
 #'   formula provides a quick way of plotting the variables in data frames,
-#'   either only one variable or a pair of variables. In the future, the
-#'   latter might be extended to a pairs display for data frames with
-#'   more than two variables but this is not implemented, yet. See the
-#'   examples for illustrations.
+#'   either only one variable or a pair of variables, or all possible pairs
+#'   of variables.
 #'
 #' @param x an object of class `"data.frame"`.
 #' @param formula a \code{\link[stats]{formula}} that is passed on to
@@ -23,36 +21,69 @@
 #' @examples
 #' tinytheme("clean2")
 #' 
-#' ## using tinyplot() with data frames and pipes
-#' cars |> tinyplot()
-#' iris |> tinyplot(Sepal.Length ~ Petal.Width | Species)
+#' ## using tinyplot() with data frames
+#' tinyplot(cars)
+#' tinyplot(iris, Sepal.Length ~ Petal.Width | Species)
 #'
-#' ## tinyplot(df) only works for data frames with 1 or 2 variables
-#' ## in the future we might add a pairs-style display as follows
-#' ## but this would require better handling of axes and their labels
-#' par(mfrow = c(5, 5))
-#' for (i in names(iris)) for(j in names(iris)) tinyplot(iris[, unique(c(j, i)), drop = FALSE])
+#' ## note that this also enables usage with pipes (in R >= 4.1.0) such as
+#' ## cars |> tinyplot()
+#' ## iris |> tinyplot(Sepal.Length ~ Petal.Width | Species)
+#'
+#' ## pairs-style display for more than 2 variables
+#' ## (handling of axes and their labels will be improved in future versions)
+#' tinyplot(iris)
 #'
 #' tinytheme() ## reset
 #'
+#' @importFrom stats reformulate
 #' @export
 tinyplot.data.frame = function (x, formula = NULL, ...) {
   ## original call
   cl = match.call()
 
-  ## default formula
-  ## 1d: y ~ 1
-  ## 2d: y ~ x
-  ## 3d: pairs? (not supported, yet)
-  if (is.null(formula)) {
-    if (ncol(x) > 2L) stop("'formula' is missing with no default for more than 2 columns")
-    f = names(x)
-    if (length(f) < 2L) f = c("1", f)
-    cl$formula = as.formula(paste(f[2L:1L], collapse = " ~ "))
-  }
-
-  ## evaluate updated call
+  ## update call for formula interface
   names(cl)[names(cl) == "x"] = "data"
   cl[[1L]] = quote(tinyplot::tinyplot)
-  eval.parent(cl)
+  if (is.null(formula)) cl$formula = . ~ .
+  id = which(names(cl) == "formula")
+  cl = as.call(as.list(cl)[c(1L, id, setdiff(2L:length(cl), id))])
+  
+  ## variables
+  nm = names(x)
+  n = length(nm)
+
+  if (is.null(formula) & n > 2L) {
+
+    ## 3d: pairs-esque  
+    op = par(mfrow = c(n, n))
+    for (j in 1L:n) {
+      for (i in 1L:n) {
+        cl_ij = cl
+        if (i == j) {
+          cl_ij$formula = reformulate("1", nm[i])
+          cl_ij$xlab = ""
+          cl_ij$ylab = ""
+          cl_ij$main = nm[i]
+        } else {
+          cl_ij$formula = reformulate(nm[i], nm[j])
+          if (i > 1L) cl_ij$ylab = ""
+          if (j < n)  cl_ij$xlab = ""
+        }
+        eval.parent(cl_ij)
+      }
+    }
+    par(op)
+
+  } else {
+
+    ## default formula
+    ## 1d: y ~ 1
+    ## 2d: y ~ x
+    if (is.null(formula)) {
+      cl$formula = if (length(nm) < 2L) reformulate("1", nm) else reformulate(nm[1L], nm[2L])
+    }
+
+    ## evaluate updated call
+    eval.parent(cl)
+  }
 }

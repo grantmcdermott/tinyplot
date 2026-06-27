ba0775f7e8ebc15c62800aaaca319b9dd5b77d4a
Grant McDermott <gmcd@amazon.com>
2026-06-26 17:13:29 -0700

    gallery updates

--- vignettes/gallery_figs/ellipse-penguins.R
+++ vignettes/gallery_figs/ellipse-penguins.R
@@ -6,7 +6,7 @@ plt(
   main = 'A waddle of penguins',
   sub = 'Species stick together',
   theme = 'web',
-  legend = list('top!', title = NULL)
+  legend = list('top!', title = FALSE)
 )
 plt_add(type = 'ellipse', fill = 0.1)
 # plt_add(type = 'chull', fill = 0.1, col = NA) ## another option

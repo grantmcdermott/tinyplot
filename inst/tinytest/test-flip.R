773adcd76dd7c5ac794500076cf6d6e73475c77f
Grant McDermott <grant.mcdermott@gmail.com>
2026-09-16 11:02:33 -0700

    test

--- inst/tinytest/test-flip.R
+++ inst/tinytest/test-flip.R
@@ -140,3 +140,14 @@ f = function() {
   )
 }
 expect_snapshot_plot(f, label = "flip_type_h_grouped")
+
+
+# flipped straight line types (#733)
+
+f = function() {
+  tinyplot(mpg ~ wt, data = mtcars, flip = TRUE, main = "Flipped h/v/ablines")
+  tinyplot_add(type = type_hline(20), col = "hotpink")
+  tinyplot_add(type = type_vline(3), col = "dodgerblue")
+  tinyplot_add(type = type_abline(a = 37, b = -5), lty = 2)
+}
+expect_snapshot_plot(f, label = "flip_ablines")

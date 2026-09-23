4403ae5ae89e5fc3035a9faeafe0a9da4aa83a49
Grant McDermott <grant.mcdermott@gmail.com>
2026-09-23 15:09:58 -0700

    test

--- inst/tinytest/test-tinytheme.R
+++ inst/tinytest/test-tinytheme.R
@@ -251,6 +251,16 @@ f = function() {
 }
 expect_snapshot_plot(f, label = "tinytheme_dynmar_mar_override")
 
+# Multi-line x tick labels hang below the ticks under dynmar
+f = function() {
+  plt(
+    x = c("Hello", "Hello\nWorld"), y = c(1, 1),
+    type = "p",
+    theme = "dynamic", xlab = NA
+  )
+}
+expect_snapshot_plot(f, label = "tinytheme_dynmar_multiline_xlabs")
+
 ## palette functions (#593)
 pal = colorRampPalette(c("darkblue", "deeppink", "cornsilk"))
 
